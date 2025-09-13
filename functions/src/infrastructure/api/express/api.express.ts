bba8af9904e74e5537d6d166e570d17f71f404af
Leonardo Benitez <>
2025-09-13 01:35:34 -0500

    update config

--- functions/src/infrastructure/api/express/api.express.ts
+++ functions/src/infrastructure/api/express/api.express.ts
@@ -22,6 +22,11 @@ export class ApiExpress implements Api {
             }
             res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+            if (req.method === 'OPTIONS') {
+                return res.sendStatus(204);
+            }
+
             next();
         });
 
